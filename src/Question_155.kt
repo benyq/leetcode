237075f76c251406200fb6b5204af43d29f277c2
benyq <1520063035@qq.com>
2020-05-31 18:46:13 +0800

    20200531

---
+++ src/Question_155.kt
@@ -0,0 +1,11 @@
+/**
+ * @author benyq
+ * @emil 1520063035@qq.com
+ * create at 2020/1/2
+ * description:
+ * 2020/05/24
+ * 题目 155
+ * https://leetcode-cn.com/problems/min-stack/description/?utm_source=LCUS&utm_medium=ip_redirect_q_uns&utm_campaign=transfer2china
+ * 这个题目简单版 只要在pop 与 push的时候操作就行
+ */
+
